266bf17a40147a3526f83816e4c01015d56d85a5
Ronan Morais <ronan86@gmail.com>
2023-09-15 10:24:36 +0930

    cards

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="astro/client" />
+interface ImportMetaEnv {
+  readonly XATA_API_KEY: string;
+  readonly XATA_BRANCH?: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
