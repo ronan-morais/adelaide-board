52a3df94bcd9147176e7c30bcd5caccb514bc4d5
Ronan Morais <ronan86@gmail.com>
2023-09-18 11:08:47 +0930

    masonry

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -6,16 +6,16 @@ export default function Card(props) {
   console.log("props", props.needs)
 
   return (
-    <div class="m-2 cursor-pointer h-fit max-w-full bg-white rounded-lg hover:shadow-md hover:border-opacity-0 transform hover:-translate-y-1 transition-all duration-200">
-      <div class="m-6">
+    <div class="cursor-pointer mb-4 break-inside-avoid-column max-w-full bg-white rounded-lg hover:shadow-md hover:border-opacity-0 transform hover:-translate-y-1 transition-all duration-200">
+      <div class="p-6">
         <div class="flex mb-2">
           <h2 class="w-full leading-5 text-md font-bold">{props.title} lorem ipsum dolor sit amet</h2>
           {props.image && (<div><IoImageOutline class="w-6 h-6 ml-5 text-slate-300" /></div>)}
         </div>
-        <p class="font-light font-mono text-xs text-gray-700 hover:text-gray-900 transition-all duration-200">{props.shortDesc}</p>
+        <p class="font-light font-mono text-xs sm:text-sm text-gray-700 hover:text-gray-900 transition-all duration-200">{props.shortDesc}</p>
         <div class="flex flex-col border-t-2 mt-2 pt-2">
-          <div class="flex flex-row items-center text-xs sm:text-sm font-mono tracking-tigh font-bold text-slate-500"><TbLocation class="w-4 h-4 mr-1 text-slate-300" /> <span class="font-normal">{props.location}</span></div>
-          <div class="flex flex-row items-center text-xs sm:text-sm font-mono tracking-tigh font-bold text-slate-500"><TbClock class="w-4 h-4 mr-1 text-slate-300" /> <span class="font-normal">{props.type}</span></div>
+          <div class="flex flex-row items-center text-xs sm:text-sm font-mono tracking-tigh font-bold text-slate-500"><TbLocation class="w-4 h-4 mr-1 text-slate-500" /> <span class="font-normal">{props.location}</span></div>
+          <div class="flex flex-row items-center text-xs sm:text-sm font-mono tracking-tigh font-bold text-slate-500"><TbClock class="w-4 h-4 mr-1 text-slate-500" /> <span class="font-normal">{props.type}</span></div>
           <div class='pt-2'>
             {props.needs.map(prop => (
               <span class="tag">{prop}</span>
